64d6362e0d1581df864939c4c1ead8cf88f8f8f1
dmamanipar <dmamanipar@unsa.edu.pe>
2023-04-19 10:40:02 -0500

    Proyecto de tipo maven Descktop

---
+++ SysGUIG3/src/main/java/pe/edu/upeu/app/FirstSwingExample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app;
+
+/**
+ *
+ * @author Data_Science
+ */
+import javax.swing.*;
+
+public class FirstSwingExample {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();//creating instance of JFrame  
+
+        JButton b = new JButton("click");//creating instance of JButton  
+        b.setBounds(130, 100, 100, 40);//x axis, y axis, width, height  
+
+        f.add(b);//adding button in JFrame  
+
+        f.setSize(400, 500);//400 width and 500 height  
+        f.setLayout(null);//using no layout managers  
+        f.setVisible(true);//making the frame visible  
+    }
+}
